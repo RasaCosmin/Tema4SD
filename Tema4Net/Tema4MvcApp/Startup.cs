a5145948e0545acd14154d5e8d7952be7df094e2
Rasa Cosmin <rasa.cosmin@gmail.com>
2015-12-29 14:23:37 +0200

    initial commit with .net project

---
+++ Tema4Net/Tema4MvcApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Tema4MvcApp.Startup))]
+namespace Tema4MvcApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
